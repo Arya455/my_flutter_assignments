cdcb3cf04aab9507e2b651590e499029eac11823
Arya455 <aryasaseendran75@gmail.com>
2023-06-30 18:00:47 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,41 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'login.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-  home: FirstPage(),));
+  home: SplashPage(),));
 }
 
-class FirstPage extends StatelessWidget{
+class SplashPage extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     body: Container(
-       decoration: BoxDecoration(
+  State<SplashPage> createState() => _SplashPageState();
+}
 
-           image: DecorationImage(fit: BoxFit.fill,
-               image: NetworkImage("https://media.istockphoto.com/id/1155415789/photo/coral-living-wave-background-marble-ombre-pattern-prism-effect-abstract-peachy-orange-red.jpg?s=612x612&w=0&k=20&c=7Q0u03O9zbljBzze81juLJxeXAZcC5uuypODxTv3T2M="))),
+class _SplashPageState extends State<SplashPage> {
+  @override
+
+
+  @override
+
+  Widget build(BuildContext context) {   return Scaffold(
+     body: Container(
+       decoration: const BoxDecoration(
+
+           image: DecorationImage(fit: BoxFit.cover,
+               //image: NetworkImage("https://media.istockphoto.com/id/1155415789/photo/coral-living-wave-background-marble-ombre-pattern-prism-effect-abstract-peachy-orange-red.jpg?s=612x612&w=0&k=20&c=7Q0u03O9zbljBzze81juLJxeXAZcC5uuypODxTv3T2M="))),
+    image: AssetImage("assets/images/gradientc.jpg"))
+         ,),
+
      child: Center(
        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
          children: [
-         Image.asset("assets/coffee.png",height: 100,width: 100,),
+         Image.asset("assets/icons/coffee.png",height: 100,width: 100,),
 
            Text("Fresh Fruits",style: TextStyle(fontSize: 25,color: Colors.white),),
          Text("Tasty & Healthy",style: TextStyle(fontSize: 15,color: Colors.white),),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:my_flutter_assignments/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( FirstPage());
+    await tester.pumpWidget( SplashPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
