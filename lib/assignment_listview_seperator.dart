aa4468f1eeb50070aa277a58c3b3cf31925c4a3f
Arya455 <aryasaseendran75@gmail.com>
2023-07-11 18:03:13 +0530

    initial commit

---
+++ lib/assignment_listview_seperator.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Seperator(),
+  ));
+}
+class Seperator extends StatelessWidget{
+  var names=["January","February","March","April","May","June",
+    "July","August","September","October","November","December"];
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(
+
+    ),
+      body: ListView.separated(itemBuilder: (context,index){
+        return Padding(
+          padding: const EdgeInsets.all( 5.0),
+          child: SizedBox(height: 45,
+            child: Card(
+              child:Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Text(names[index],style: TextStyle(color: Colors.black),),
+              ) ,
+            ),
+          ),
+        );
+      }, separatorBuilder: (context, index){
+        if(index%4==0) {
+          return  Padding(
+            padding:  EdgeInsets.all(05.00),
+            child: SizedBox(height: 40,
+              child: Card(
+                color: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text("Advertisement",style: TextStyle(color: Colors.white,),),
+                ),
+
+
+              ),
+            ),
+          );
+
+      };
+
+        return Text("");
+      }, itemCount: 12),
+    );
+  }
+}
--- lib/whatsappui.dart
+++ lib/whatsappui.dart
@@ -14,7 +14,165 @@ class WhatsApp extends StatelessWidget{
 return Scaffold(
   appBar: AppBar(
     title: Text("What'sApp"),
+    actions: const [
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Icon(Icons.search),
+      ),
+      SizedBox(width: 10,),
+      Padding(
+        padding: EdgeInsets.only(right: 30.0),
+        child: Icon(Icons.camera_alt),
+      ),
+    ],
   ),
+  body: ListView(
+    children:   [
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(
+                  "assets/images/images.jpg")
+            ),
+    title: Text("Priyanka Chopra"),
+    subtitle: Text("hai, hello"),
+    trailing:Column(
+
+      children: [
+
+        Text("10:15"),
+        Icon(Icons.done_all)
+      ],
+    )
+
+            
+            ),
+          ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/nick.jpg"
+                   )
+            ),
+            title: Text("Nick Jonas"),
+            subtitle: Text("hai, kya he"),
+            trailing:SizedBox(
+              child: Column(
+                children: [Text("10.00"),
+                  Icon(Icons.done_all)
+                ],
+              ),
+            )
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "assets/images/prabhas.jpg")
+            ),
+            title: Text("Prabhas"),
+            subtitle: Text("hai, whatsapp"),
+            trailing: Text("10.00"),
+
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "assets/images/nayan.jpg")
+            ),
+            title: Text("NayanThara"),
+            subtitle: Text("hai, whatsapp"),
+            trailing: Text("09:59"),
+
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "assets/images/katrina.jpg")
+            ),
+            title: Text("Katrina Kaif"),
+            subtitle: Text("hai, whatsapp"),
+            trailing: Text("09:55"),
+
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "assets/images/trisha.jpg")
+            ),
+            title: Text("Trisha"),
+            subtitle: Text("Are you ok????"),
+            trailing: Text("09:50"),
+
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "assets/images/sharukh.jpg")
+            ),
+            title: Text("Sharukh Khan"),
+            subtitle: Text("hai, how are you"),
+            trailing: Text("09:45"),
+
+
+
+
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "assets/images/shilpa.jpg")
+            ),
+            title: Text("Shilpa"),
+            subtitle: Text("hai, where are you"),
+            trailing: Column(
+              children: [
+                Text("yesterday")
+              ],
+            )
+
+          ),
+        ),
+      ),
+
+
+    ],
+  ),
+  
 );
   }
 }
